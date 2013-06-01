ac643eb9ae4424de13ee6025058c82935a3471ce
John <john@SMYAYA-MacBook-Pro.local>
2013-06-01 12:27:15 -0400

    testing 123

---
+++ testing 2/testing 2/smyayaViewController.h
@@ -0,0 +1,13 @@
+//
+//  smyayaViewController.h
+//  testing 2
+//
+//  Created by John on 6/1/13.
+//  Copyright (c) 2013 John. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface smyayaViewController : UIViewController
+
+@end
